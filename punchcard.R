2f0ed8efc3435b0fc6d54c7eb9dc137799c72827
Matt Tierney <tierney@cs.nyu.edu>
2012-04-02 10:16:46 -0400

    Git punchcard analog in R.

--- punchcard.R
+++ punchcard.R
@@ -1,8 +1,8 @@
 library("ggplot2")
 library("reshape")
 library("digest")
-
-SOURCE=Sys.glob('/home/power/.thunderbird/*/*/*/*/Sent Mail')
+library("plyr")
+library("scales")
 
 lazy = function(expr) {
  if (!exists("cache")) { assign("cache", new.env(), envir=.GlobalEnv)}
@@ -19,23 +19,38 @@ getMatches = function(data) {
  return(gsub('Date:.*, (.*-0\\d+).*', '\\1', matched_lines))
 }
 
-lines = lazy(readLines(SOURCE))
-matches = lazy(getMatches(lines))
-dates = lazy(lapply(matches, function(f) {strptime(f, format="%d %b %Y
-%H:%M:%S %z")}))
-df = ldply(dates, unlist)
-df$year = df$year + 1900
-df_counts = count(df, vars=c("year", "hour"))
+# Set to git repo here.
+TARGET_REPO = '/home/tierney/repos/libjingle/'
 
-for (year in df_counts$year) {
- m = df_counts$year == year
- df_counts$freq[m] = df_counts$freq[m] / sum(df_counts$freq[m])
+lines=readLines(pipe(paste('cd', TARGET_REPO, '&& git log | grep Date && cd -')))
+
+gitMatches= function(data) {
+  matched_lines = grep('^Date: .*[-+]0\\d+', data, value=T)
+  return(gsub('Date: *(.*[-+]0\\d+).*', '\\1', matched_lines))
+}
+matches = gitMatches(lines)
+
+# 'Thu Mar 15 23:39:39 2012 -0400'
+gitDateFormat <- '%a %b %d %H:%M:%S %Y %z'
+thunderbirdDateFormat <- "%d %b %Y %H:%M:%S %z"
+dates = lapply(matches, function(f) {strptime(f, format=gitDateFormat)})
+
+df = ldply(dates, unlist)
+#df$wday = df$wday + 1900
+df_counts = count(df, vars=c("wday", "hour"))
+
+for (wday in df_counts$wday) {
+ m = df_counts$wday == wday
+ df_counts$rel_freq[m] = df_counts$freq[m] / sum(df_counts$freq)
 }
 
-p = ggplot(df_counts, aes(x=year, y=hour)) +
- scale_x_continuous(limits=c(2004, 2012)) +
- scale_y_datetime() +
- geom_tile(aes(fill=freq)) +
- scale_fill_gradient()
+days_of_the_week = c('Sunday','Monday','Tuesday','Wednesday',
+                     'Thursday','Friday','Saturday')
+
+p = ggplot(df_counts, aes(x=hour, y=wday)) +
+ scale_y_continuous(name='', limits=c(0,6),
+                    labels=factor(seq(0,6,1), labels=days_of_the_week)) +
+ scale_x_continuous(name='Hour', limits=c(0,23), breaks=seq(0,23,1)) +
+ geom_point(aes(size=rel_freq))
 
 show(p)
